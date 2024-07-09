b78ee2a8a9f6484bf568012e0c3dc0dafb9d5e6f
Yousif Magdi <yousifmagdi@gmail.com>
2024-07-09 09:04:37 -0400

    test find by availability

--- tests/test_models.py
+++ tests/test_models.py
@@ -181,4 +181,16 @@ class TestProductModel(unittest.TestCase):
     
     def test_find_a_product_by_availability(self):
         """ It shoud Find a Product by Availability """
+        # create the products
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        
+        available = products[0].available
+        count = len([product for product in products if product.available == available])
+        found = Product.find_by_availability(available)
+        self.assertEqual(found.count(), count)
+
+        for product in found:
+            self.assertEqual(product.available, available)
         
